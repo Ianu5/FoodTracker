96fa0ad7de12275deb0550597e650484c37aa033
Jesaja Hur <j.hur@iqul.de>
2024-07-02 10:21:04 +0200

    Added todo for csv handler

--- CSVHandler.py
+++ CSVHandler.py
@@ -2,10 +2,17 @@ import csv
 import os
 import gzip
 
+
+# TODO I will need to write the code for downloading the csv file automatically
+# and placing it in the right folder for usage
 class CSVHandler:
     def __init__(
             self
     ):
+        """
+        This class handles the csv file from openfoodfacts and returns an
+        iterator which shows the data in dictionary format.
+        """
         self.filename = 'en.openfoodfacts.org.products.csv.gz'
         self.directory = os.getcwd()
         self.folder = 'food_databank'
