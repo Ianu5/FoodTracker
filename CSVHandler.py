185c6ff0956ec01bda4356aebf5a221be83d3e15
Jesaja Hur <j.hur@iqul.de>
2024-07-04 09:33:51 +0200

    Added functionality for the class to download the required dataset automatically if the dataset does not exist

--- CSVHandler.py
+++ CSVHandler.py
@@ -1,24 +1,35 @@
 import csv
 import os
 import gzip
+import openfoodfacts
+from pathlib import Path
 
 
-# TODO I will need to write the code for downloading the csv file automatically
-# and placing it in the right folder for usage
 class CSVHandler:
-    def __init__(
-            self
-    ):
+    def __init__(self):
         """
         This class handles the csv file from openfoodfacts and returns an
         iterator which shows the data in dictionary format.
         """
+
+        # Specifying the path in which the file is stored
         self.filename = 'en.openfoodfacts.org.products.csv.gz'
         self.directory = os.getcwd()
         self.folder = 'food_databank'
-        self.filepath = os.path.join(self.directory, self.folder, self.filename)
+        self.filepath = os.path.join(self.directory,
+                                     self.folder,
+                                     self.filename)
+
+        # If the file does not exist download it into the food_databank folder
+        self.download_path = Path('./food_databank')
+        if not os.path.exists(self.filepath):
+            self.dataset = openfoodfacts.ProductDataset(dataset_type='csv',
+                                                    cache_dir=self.download_path)
 
     def __iter__(self):
+        """
+        Method to return an iterator for the items in the dataset
+        """
         with gzip.open(self.filepath, mode='rt', encoding='utf-8', newline='') as file:
             reader = csv.DictReader(file, delimiter='\t')
             for row in reader:
