2f35c2ae0d3f4deaba8a06e8aa2e7794803b1119
Jesaja Hur <j.hur@iqul.de>
2024-07-12 12:12:58 +0200

    Added comments to the Menu class to make it easier to work with

--- MenuClass.py
+++ MenuClass.py
@@ -3,17 +3,20 @@ import sys
 
 
 class Menu:
+    """Base Menu class for usage in more elaborate menus"""
     def __init__(self, *options):
         self.options = options
         self.choice = None
 
     def display(self):
+        # Method to display all the options in the menu
         for index, item in enumerate(self.options, start=1):
             print(index, item)
         print()
         return None
 
     def set_choice(self):
+        # method to setting the choice the user inputs as a instance variable
         self.choice = input("Choice: ")
         if int(self.choice) not in range(len(self.options) + 1):
             print("Not a valid option")
@@ -23,25 +26,45 @@ class Menu:
 
 
 class EntryMenu(Menu):
+    """This class inherits from the menu class and is the first menu the 
+    user sees and interacts with. It handles the Users as in choosing,
+    adding and deleting users from the Userdatabank"""
+
+    # Options the menu can give the user
     options = ("Choose user", "Add user", "Delete user", "Exit")
     def __init__(self):
         self.options = EntryMenu.options
 
     def display(self):
+        # Inherits the display method from the Menu class this renders
+        # the available options for the user
         return super().display()
     
     def set_choice(self):
+        # Sets the choice from the user as an instance attribute
         return super().set_choice()
         
     def handle_choice(self, userdatabank: UserDatabank):
+        # Acting on the choice of the user
+        # initializing an instance of our user databank
         users = userdatabank.return_users()
+
+        # Using match case to handle the input of the user
         match self.choice:
             case '1':
+                # In case the user chooses his account he will be forwarded
+                # to the Usermenu
                 userdatabank.show_users()
                 if len(users) != 0:
                     choice = int(input("Choose: "))
                     if 1 <= choice <= len(users):
-                        print(users[choice - 1])
+                        user = users[choice - 1]
+                        print(f"\nHello {user} what do you want to do?\n")
+                        menu = UserMenu(user)
+                        menu.display()
+                        menu.set_choice()
+                        menu.handle_choice()
+
             case '2':
                 userdatabank.add_user()
             case '3':
@@ -53,12 +76,28 @@ class EntryMenu(Menu):
 
 
 class UserMenu(Menu):
-    options = ("Search food", "Enter food eaten", "Display eating history", "Add recipie")
-    def __init__(self):
-        self.options = UserMenu.options()
+    options = ("Search food", "Enter food eaten", "Display eating history", "Add recipie", 'Exit')
+    def __init__(self, user):
+        self.options = UserMenu.options
+        self.user = user
 
     def display(self):
         return super().display()
     
     def set_choice(self):
         return super().set_choice()
+
+    def handle_choice(self):
+        match self.choice:
+            case '1':
+                # TODO write a class for food info handling
+                ...
+            case '2':
+                # TODO write a class for tracking eaten calories
+                ...
+            case '3':
+                # TODO method in calorie tracker that displays the data
+                ...
+            case '4':
+                # TODO refer to the Cookbook class here
+                ...
