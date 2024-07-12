949ed48b462b455c0e8885c5e03decf903f847c3
Jesaja Hur <j.hur@iqul.de>
2024-07-12 13:54:52 +0200

    Added search food method to Fooddatabankclass for looking up food the user searches for

--- FoodDataBankClass.py
+++ FoodDataBankClass.py
@@ -5,13 +5,25 @@ import openfoodfacts
 from pathlib import Path
 
 
-class CSVHandler:
+class FoodData:
+    """This class handles the csv file from openfoodfacts and returns an
+    iterator which shows the data in dictionary format."""
+    fields = [
+        'code',
+        'url',
+        'product_name',
+        'abbreviated_product_name',
+        'generic_name',
+        'quantity',
+        'brands',
+        'energy-kj_100g',
+        'energy-kcal_100g',
+        'energy_100g',
+        'unsaturated-fat_100g',
+        'carbohydrates_100g',
+        'sugars_100g',
+        ]
     def __init__(self):
-        """
-        This class handles the csv file from openfoodfacts and returns an
-        iterator which shows the data in dictionary format.
-        """
-
         # Specifying the path in which the file is stored
         self.filename = 'en.openfoodfacts.org.products.csv.gz'
         self.directory = os.getcwd()
@@ -19,6 +31,7 @@ class CSVHandler:
         self.filepath = os.path.join(self.directory,
                                      self.folder,
                                      self.filename)
+        self.fields = FoodData.fields
 
         # If the file does not exist download it into the food_databank folder
         # Using openfoodfacts python sdk for downloading the file
@@ -35,3 +48,11 @@ class CSVHandler:
             reader = csv.DictReader(file, delimiter='\t')
             for row in reader:
                 yield(dict(row))
+
+    def search_food(self, name: str, type): #TODO how do I store the result of the search in what format do I return the shit
+        # The type is for setting by which parameter we are going to search the databank by
+        for product in self:
+            if product['product_name'] == name:
+                for i in range(len(self.fields)):
+                    if product[self.fields[i]]: # TODO I will have to change the print statement to something so I can return this information
+                        print(f'{self.fields[i]}: {product[self.fields[i]]}')
